60fe46076db7961af4871d44da10fe5a8e700915
Pnevsk123 <vlad.007.p@gmail.com>
2017-06-04 23:42:57 +0300

    first commit

---
+++ Script/Buton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Buton : MonoBehaviour {
+
+    public GameObject panel;
+
+    public void Button()
+    {
+        panel.SetActive(true);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Script/DestroyOnContact.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DestroyOnContact : MonoBehaviour
+{
+    public GameObject explosion;
+    public GameObject playerExplosion;
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        {
+            Destroy(gameObject);
+            Destroy(collision.gameObject);
+
+    if ( collision.gameObject.tag.Equals("Player"))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        Instantiate(explosion, transform.position, transform.rotation);
+    }
+}
